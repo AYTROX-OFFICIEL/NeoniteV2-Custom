c21891f6776eeb5840ea6c9f2a7fe7edd071b305
AYTROX <74268308+AYTROX-OFFICIEL@users.noreply.github.com>
2022-03-04 22:37:04 +0100

    Delete consoleoutput.js

--- aytrox/consoleoutput.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    log: data => console.log(`[\x1b[31mGénérateur\x1b[0m] ${data}\x1b[0m`),
-    request: data => console.log(`[\x1b[31mREQUEST\x1b[0m] ${data}\x1b[0m`),
-    warn(data, showtype = true) {
-        console.warn('[\x1b[33mWARNING\x1b[0m]\x1b[33m', data, '\x1b[0m')
-      }
-}
